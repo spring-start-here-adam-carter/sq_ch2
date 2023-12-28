c8c7506d9681a20546b44808b05cabca1c009820
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-28 15:54:36 -0500

    Define a configuration class and create a method that returns the bean

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import main.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+}
