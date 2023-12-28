3bce610c8620f00f83c4513cec0ad52492722f70
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-28 17:17:28 -0500

    Use stereotype annotations to add beans to the Spring context

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -2,31 +2,11 @@ package config;
 
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "main")
 public class ProjectConfig {
-
-    @Bean
-    Parrot parrot1() {
-        var p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
-
-    @Bean
-    @Primary
-    Parrot parrot2() {
-        var p = new Parrot();
-        p.setName("Miki");
-        return p;
-    }
-
-    @Bean
-    Parrot parrot3() {
-        var p = new Parrot();
-        p.setName("Riki");
-        return p;
-    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,6 +8,8 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p);
         System.out.println(p.getName());
     }
 }
