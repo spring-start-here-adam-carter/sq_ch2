e89f30bbc98cfc9b5520692a91cea6d604a9479c
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-28 16:04:01 -0500

    Add two more beans to its context

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -13,4 +13,14 @@ public class ProjectConfig {
         p.setName("Koko");
         return p;
     }
+
+    @Bean
+    String hello() {
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten() {
+        return 10;
+    }
 }
