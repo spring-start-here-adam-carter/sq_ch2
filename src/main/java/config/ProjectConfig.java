2331db3577aa5c6608f248c2df8e580ef387b34c
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-28 17:06:10 -0500

    Make one of the beans primary

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package config;
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
@@ -14,7 +15,8 @@ public class ProjectConfig {
         return p;
     }
 
-    @Bean(name = "miki")
+    @Bean
+    @Primary
     Parrot parrot2() {
         var p = new Parrot();
         p.setName("Miki");
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot p = context.getBean("miki", Parrot.class);
+        Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
     }
 }
