ed6edc025040faa0aa60468769c359d9ed0c363c
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-28 16:59:09 -0500

    Declare many beans of the same time in the configuration class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,13 +7,7 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot p = context.getBean(Parrot.class);
+        Parrot p = context.getBean("parrot2", Parrot.class);
         System.out.println(p.getName());
-
-        String s = context.getBean(String.class);
-        System.out.println(s);
-
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
     }
 }
